4885835ebbc88a5ba8b73888a52e6bcf80410e95
eserubio <guillermo_rubio@live.com>
2016-01-11 21:46:26 +0100

    menu defectuoso

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -27,18 +27,66 @@ export default class App extends Component {
 
   render() {
     return (
-     <div>
-        <nav className="navbar navbar-inv">
-            <ul className="nav navbar-nav">
-              <li onClick={() => this.handleLogoOK()}><Link to="/">Home</Link></li>
-              <li onClick={() => this.handleLogo()}><Link to="/preferences">Preferences</Link></li>
-              <li onClick={() => this.handleLogo()}><Link to="/selecteds">Yours Concerts</Link></li>
-            </ul>
-        </nav>
-        <img className={`${this.state.logo ? 'logo' : 'hidden' }`}  src={image}/>
+
+
+
+<div>
+  <nav className="navbar navbar-default" role="navigation">
+   <Link to="/"><img className="navbar-brand"  src={image}/></Link>
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle" data-toggle="collapse"
+              data-target=".navbar-ex1-collapse">
+        <span className="sr-only">Desplegar navegación</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+     
+    </div>
+   
+
+    <div className="collapse navbar-collapse navbar-ex1-collapse">
+      <ul className="nav navbar-nav">
+        <li><a href="#">Enlace #1</a></li>
+        <li onClick={() => this.handleLogo()}><Link to="/preferences">Preferences</Link></li>
+      </ul>
+   
+      <ul className="nav navbar-nav navbar-right">
+        <li onClick={() => this.handleLogo()}><Link to="/selecteds">Yours Concerts</Link></li>
+      </ul>
+    </div>
+  </nav>
+    <img className={`${this.state.logo ? 'logo' : 'hidden' }`}  src={image}/>
         {this.props.children}
-      </div>
-    );
+</div>
+
+
+
+      /*
+     <nav className="navbar navbar-default" role="navigation">>
+       <div className="navbar-header">
+         <button type="button" className="navbar-toggle" data-toggle="collapse"
+         data-target=".navbar-ex1-collapse">
+           <span className="sr-only">Desplegar navegación</span>
+           <span className="icon-bar"></span>
+           <span className="icon-bar"></span>
+           <span className="icon-bar"></span>
+         </button>
+         <a className="navbar-brand" href="#">Logotipo</a>
+       </div>
+       <div>
+         <nav className="navbar navbar-inv">
+         <ul className="nav navbar-nav">
+           <li onClick={() => this.handleLogoOK()}><Link to="/">Home</Link></li>
+           <li onClick={() => this.handleLogo()}><Link to="/preferences">Preferences</Link></li>
+           <li onClick={() => this.handleLogo()}><Link to="/selecteds">Yours Concerts</Link></li>
+         </ul>
+         </nav>
+         <img className={`${this.state.logo ? 'logo' : 'hidden' }`}  src={image}/>
+         {this.props.children}
+       </div>
+     </nav>
+    */);
   }
 }
 

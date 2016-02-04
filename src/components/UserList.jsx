df43af29cbd1bc8ad6688d25a39d661241b93b81
cricrespo <cricrespo@gmail.com>
2016-02-04 12:36:33 +0100

    userlist, hide navbar

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,42 +4,70 @@ export default class UserList extends Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      send:false
+      
+    };
   }
 
   handleSendClick(id){
     const node = this.refs[id];
     const title =  node.value.trim();
     this.props.sendMessage(title, id);
+
+    this.setState({
+      send:false
+    });
+
+  }
+  handleCancelMessage(){
+
+    this.setState({
+      send:false
+    });
+    
+  }
+
+  handleSendMessageClick(id){
+
+    this.setState({
+      send:id
+    });
   }
 
   render() {
-    const { users } = this.props;
-
+    const { users, auth } = this.props;
+    
     return (
      
       <div>
         <div>
-        <h3>users Selected</h3>
+        <h3>Users Selected</h3>
         </div>
         <div>
-          {users.length===0? <li>none</li> :
-            users.map( (user, index) => <div className=""><div key={index} className="list-group-item">
 
-              <div className="pull-left">
+            {users.length===0? <li>none</li> :
+            users.map( (user, index) => <div className="">
+            <div key={index} className={auth.id === user.id ? 'hide' : 'list-group-item'}>
+
+              <div className="text-center">
+                <div className="pull-left">
                 <img className="avatar img-circle img-thumbnail" src={user.img}/>
+                </div> 
+                <button className={`btn btn-warning pull-right ${this.state.send=== user.id ? 'hidden' : '' }`} type="button" onClick={() => this.handleSendMessageClick(user.id)}><span className="glyphicon glyphicon-envelope card" /></button>
+              
+                <p>Name: {user.name || ''}</p><p>Age: {user.age  || 'no data'}</p><p>  Description: {user.description  || ''}</p>
               </div>
-              <div className="input-group">
+              <div className={`input-group ${this.state.send === user.id  ? '' : 'hidden'}`}>
                 <input  type="text"  className="form-control" placeholder="Send a message" ref={`${user.id}`} />
                 <span className="input-group-btn">
-                  <button className="btn btn-info" type="button" onClick={() => this.handleSendClick(user.id)}><span className="glyphicon glyphicon-plus" /></button>
+                  <button className="btn btn-warning " type="button" onClick={() => this.handleSendClick(user.id)}><span className="glyphicon glyphicon-arrow-right" /></button>
+                  <button className="btn btn-warning" type="button" onClick={() => this.handleCancelMessage()}><span className="glyphicon glyphicon-minus" /></button>
                 </span>
-              </div>
-              <div className="text-center">
-                <p>Name: {user.name || ''}</p><p>Age: {user.age  || 'no data'}  Description: {user.description  || ''}</p>
               </div>
             </div><br/></div>)
           }
-         </div>
+         </div> 
       </div>
     );
   }
--- src/containers/MenuContainer.jsx
+++ src/containers/MenuContainer.jsx
@@ -6,15 +6,13 @@ import * as authActions from '../actions/auth/index.js';
 import Spinner from '../components/Spinner';
 
 
-
-
-
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      loading: true
+      loading: true,
+      hide: false
     };
   }
 
@@ -22,6 +20,8 @@ class App extends Component {
 
     this.setState({ loading: false });
   }
+
+
 
   handleOnChangeTitle() {
     const node = this.refs.title;
@@ -38,17 +38,19 @@ class App extends Component {
     this.props.signOut();
   }
 
+
+
   render() {
     const { search } = this.props;
 
-
+    
 
     return (
 
- <nav className="navbar navbar-inverse">
+<nav className="navbar navbar-inverse">
         <div className="container-fluid 2">
             <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" >
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span className="sr-only">Toggle navigation</span>
                     <span className="icon-bar"></span>
                     <span className="icon-bar"></span>
@@ -56,58 +58,68 @@ class App extends Component {
                 </button>
                <Link className="navbar-brand" to="/">ShowMustGoOn</Link>
             </div>
-
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav navbar-right">
                     <li className="hidden">
                         <a href="#page-top"></a>
                     </li>
-                     
-                    <li className="search">
-                         <input type="text" placeholder="Search..." ref="title" onChange={e => this.handleOnChangeTitle(e)}/>
-                         <ul className="results" >
-                          {
-                            search.map( (band, index) =>  <li className="list-group-item" key={index}><a href={`band/${band.id}`}>{band.title}</a></li> )
-                          }
-                         </ul>
-                    </li>
-                    
-                          <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
-                              <Link to="/messages">Messages</Link>
-                          </li> 
-                          <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
-                              <Link to="/profile">Profile</Link>
-                          </li>                    
-                          <li className={this.props.auth.authenticated?'hidden':'nav navbar-nav'}>
-                              
-                              <Link to="/sign-in">Sign-in</Link>
-                              
-                          </li>
-                          <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
-                              <Link to="/" onClick={e => this.handleSignOutButtonClick(e)}>
-                              <span className="glyphicon glyphicon-off red"/></Link>
-                          </li>
                 </ul>
-                  <ul className="nav navbar-nav">
-                    <li className="nav navbar-nav">
-                        <Link to="/preferences">Preferences</Link>
-                    </li>   
-                     <li className="nav navbar-nav">
-                        <Link to="/calendar">Calendar</Link>
-                    </li>                  
-                    <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
-                        <Link to="/selecteds">Your Concerts</Link>
-                    </li>
+                  <ul className="nav navbar-nav" >
+                      <li className="nav navbar-nav">
+                          <Link to="/preferences" className="hidden-xs">Preferences</Link>
+                          <Link to="/preferences" className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">Preferences</Link>
+                      </li>                    
+                      <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
+                          <Link to="/selecteds" className="hidden-xs">Your Concerts</Link>
+                          <Link to="selecteds" className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">Your Concerts</Link>
+                      </li>
 
-                </ul>
+                       <li className="nav navbar-nav">
+                       <Link to="/calendar" className="hidden-xs">Calendar</Link>
+                        <Link to="calendar" className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">Calendar</Link>
+                    </li> 
+
+                       <li className="search">
+                          <input type="text" placeholder="Search..." ref="title" onChange={e => this.handleOnChangeTitle(e)}/>
+                            <ul className="results" >
+                              {
+                                search.map( (band, index) =>  <li className="list-group-item" key={index}><a href={`band/${band.id}`}>{band.title}</a></li> )
+                              }
+                           </ul>
+                        </li> 
+
+                        <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
+                          <Link to="/messages" className="hidden-xs">Messages</Link>
+                          <Link to="messages" className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">Messages</Link>
+                        </li> 
+                        <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
+                          <Link to="/profile" className="hidden-xs">Profile</Link>
+                          <Link to="profile" className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">Profile</Link>
+
+                        </li>                    
+                        <li className={this.props.auth.authenticated?'hidden':'nav navbar-nav'}>
+                          <Link to="/sign-in" className="hidden-xs">Sign-in</Link>
+                          <Link to="sign-in" className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">Sign-in</Link>
+  
+                        </li>
+                        <li className={this.props.auth.authenticated?'nav navbar-nav':'hidden'}>
+                              <Link to="/" onClick={e => this.handleSignOutButtonClick(e)} className="hidden-xs">
+                              <Link to="/"  onClick={e => this.handleSignOutButtonClick(e)} className="visible-xs" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span className="glyphicon glyphicon-off red"/></Link></Link>
+                        </li> 
+                  </ul>     
+                
 
             </div>
         </div>
-        </nav>
+</nav>
+
 
     );
   }
 }
+
+  
 
 App.propTypes = {
   // Injected by React RouterConfirmDialog

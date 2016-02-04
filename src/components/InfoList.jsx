d96f19445a3295f00aa5791b1f40f35f9d834752
eserubio <guillermorubros@gmail.com>
2016-02-04 10:26:02 +0100

    add info date ok

--- src/components/InfoList.jsx
+++ src/components/InfoList.jsx
@@ -27,11 +27,11 @@ export default class InfoList extends Component {
     });
     const { addInfo, idBand } = this.props;
     const node = this.refs.place;
-    const node2 = this.refs.date;
-    const node3 = this.refs.price;
+    const node2 = this.refs.price;
     const title =  node.value.trim();
-    const date =  node2.value.trim();
-    const price =  node3.value.trim();
+    const date =  new Date(this.refs.year.value, this.refs.month.value-1, this.refs.day.value).getTime();
+debugger
+    const price =  node2.value.trim();
     if (title === '' | date === '' | price === '' ){
       alert('Missing input'); 
     }else{    
@@ -40,7 +40,9 @@ export default class InfoList extends Component {
 
     node.value = '';
     node2.value = '';
-    node3.value = '';
+    this.refs.year.value='';
+    this.refs.month.value='';
+    this.refs.day.value='';
   }
 
   handelOnclickAdd(){
@@ -65,13 +67,15 @@ export default class InfoList extends Component {
                                               'hidden'
                                               :infor.users[auth.id]===undefined||!infor.users[auth.id].assist?
                                                 'btn btn-success glyphicon glyphicon-log-in pull-right action-icon':'btn btn-warning glyphicon glyphicon-log-out pull-right action-icon'}
-                                               ></span><p key={index}>{infor.title} on {infor.date} at {infor.price}€</p>
+                                               ></span><p key={index}>{infor.title} on {new Date(infor.date).getDate()}/{new Date(infor.date).getMonth()+1}/{new Date(infor.date).getFullYear()} at {infor.price}€</p>
                                           </li> )
             }
             <br/>
           <div className={` ${this.state.editting ? 'input-group '  : 'hidden'}` }>
             <input  type="text"  className="form-control" placeholder="Add place" ref="place" />
-            <input  type="date"  className="form-control" placeholder="Add date" ref="date" />
+            <input  type="number"  className="form-control" placeholder="Add day" ref="day" />
+            <input  type="number"  className="form-control" placeholder="Add month" ref="month" />
+            <input  type="number"  className="form-control" placeholder="Add year(yyyy)" ref="year" />
             <input  type="number"  className="form-control" placeholder="Add price" ref="price" />
             <span className="input-group-btn">
               <button className="btn btn-info" type="button" onClick={e => this.handleAddButtonClick(e)}><span className="glyphicon glyphicon-plus" /></button>

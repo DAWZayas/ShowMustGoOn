894ddf1094f4976343df05a75a95c4d7dbb67b9c
cricrespo <cricrespo@gmai.com>
2016-02-11 12:34:00 +0100

    a

--- src/components/ConcertsSelected.jsx
+++ src/components/ConcertsSelected.jsx
@@ -16,7 +16,7 @@ export default class ConcertsSelected extends Component {
         <h3>Concerts Selected</h3>
         </div>
           {selecteds.length===0? <li>none</li> :
-            selecteds.map( (concert, index) =>  <Link key={index} className="list-group-item action-element" to={`users/${concert.id}`}><p className="pull-left"><b>Band:</b>  {concert.bandName}</p><p className="pull-right"><b>Place:</b>  {concert.title}</p><p><b>Date:</b>  {concert.date}</p></Link>)
+            selecteds.map( (concert, index) =>  <Link key={index} className="list-group-item action-element" to={`users/${concert.id}`}><p className="pull-right"><b>Band:</b>  {concert.bandName}</p><p className="pull-left"><b>Place:</b>  {concert.title}</p><p className="text-center"><b>Date:</b>  {concert.date}</p></Link>)
           }
       </div>
     );
